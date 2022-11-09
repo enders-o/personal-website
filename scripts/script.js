375dedc0f33f534d5a9a6217e140e8973cb4f246
Ender's Laptop <ender_sotelo@outlook.com>
2022-11-09 15:32:53 -0800

    Created a const array called courseList with 3 object items

---
+++ scripts/script.js
@@ -0,0 +1,16 @@
+const courseList = [
+    {
+        "code": "ACIT 1620",
+        "name": "Fundamental Web Technologies"
+    },
+    {
+        "code": "ACIT 1630",
+        "name": "Database Systems"
+    },
+    {
+        "code": "ACIT 1515",
+        "name": "Scripting for IT"
+    }
+]
+
+// console.log(courseList)
