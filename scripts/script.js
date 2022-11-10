dc82cb26463eed55d48bc04d94909f6d81617e9b
Ender's Laptop <ender_sotelo@outlook.com>
2022-11-09 16:08:34 -0800

    Created a for loop to check if user's input is in array, if it is change flag to 1, then flag checks if a new course is added if so add to array

--- scripts/script.js
+++ scripts/script.js
@@ -14,9 +14,26 @@ const courseList = [
 ]
 let input;
 do {
-    input = prompt("Enter a 4-digit number:") 
-    input = Number(input)
-    console.log(isNaN(input))
+    input = prompt("Enter a 4-digit number:");
+    input = Number(input);
+    console.log(isNaN(input));
 } while (isNaN(input) || input <= 1000 || input >= 9999);
+
+let flag = 0
+for (const item of courseList) {
+    if (item.code.includes(input)) {
+        console.log(`Yes I am taking the course ${item.code} - ${item.name}`);
+        flag = 1
+        break;
+    }
+};
+
+if(flag === 0) {
+    courseList.push({
+        "code": `ACIT ${input}`,
+        "name": null
+    })
+    console.log(`Added ACIT ${input} to array.`);
+}
 
 // console.log(courseList)
